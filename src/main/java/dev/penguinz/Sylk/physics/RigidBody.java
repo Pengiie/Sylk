0f1d19e2237f1753758c0e6c905262cae4c805d6
Penguinz22 <nate19522@gmail.com>
2020-10-11 19:49:47 -0700

    Make Physics Engine collision detection

--- src/main/java/dev/penguinz/Sylk/physics/RigidBody.java
+++ src/main/java/dev/penguinz/Sylk/physics/RigidBody.java
@@ -2,12 +2,26 @@ package dev.penguinz.Sylk.physics;
 
 import dev.penguinz.Sylk.util.maths.Vector2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RigidBody {
+
+    private List<Collider> colliders = new ArrayList<>();
 
     private Vector2 velocity;
 
+    public float friction = 0;
+    public float restitution = 0;
+    public float mass = 1;
+    public boolean isStatic = false;
+
     public RigidBody() {
         this.velocity = new Vector2();
+    }
+
+    public List<Collider> getColliders() {
+        return colliders;
     }
 
     public Vector2 getVelocity() {
